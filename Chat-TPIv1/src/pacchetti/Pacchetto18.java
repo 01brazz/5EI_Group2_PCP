01a8899c0f2c2f2d9ac2d9df279ae83255c64f2e
5EI <5EI@EDU.MARCONI>
2019-11-15 13:07:21 +0100

    creato pacchetto 18

--- Chat-TPIv1/src/pacchetti/Pacchetto18.java
+++ Chat-TPIv1/src/pacchetti/Pacchetto18.java
@@ -11,5 +11,69 @@ package pacchetti;
  */
 public class Pacchetto18
 {
+    private byte[] id;
+    private String old_Alias;
+    private String new_Alias;
+
+    public Pacchetto18(byte[] id,
+                       String old_Alias,
+                       String new_Alias)
+    {
+        this.id = id;
+        this.old_Alias = old_Alias;
+        this.new_Alias = new_Alias;
+    }
+
+    public byte[] getId()
+    {
+        return id;
+    }
+
+    public String getOld_Alias()
+    {
+        return old_Alias;
+    }
+
+    public String getNew_Alias()
+    {
+        return new_Alias;
+    }
+
+    public void setId(byte[] id)
+    {
+        this.id = id;
+    }
+
+    public void setOld_Alias(String old_Alias)
+    {
+        this.old_Alias = old_Alias;
+    }
+
+    public void setNew_Alias(String new_Alias)
+    {
+        this.new_Alias = new_Alias;
+    }
     
+     public byte[] createP()
+    {
+        byte[] packet = new byte[2048];
+        int i = 0;
+        packet[i++] = 18;
+        
+        for (byte b : this.id)
+        {
+            packet[i++] = b;
+        }
+        for (byte b : this.old_Alias.getBytes())
+        {
+            packet[i++] = b;
+        }
+
+        for (byte b : this.new_Alias.getBytes())
+        {
+            packet[i++] = b;
+        }
+
+        return packet;
+    }
 }
