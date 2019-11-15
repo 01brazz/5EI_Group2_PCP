6b966abcaa4f361b6c5b6c9c1c298e704a09c40a
BELTRAMI ANDREA <17701@EDU.MARCONI>
2019-11-15 12:23:13 +0100

    Creato pacchetto 01

--- Chat-TPIv1/src/pacchetti/Packet01.java
+++ Chat-TPIv1/src/pacchetti/Packet01.java
@@ -5,10 +5,79 @@
  */
 package pacchetti;
 
+import java.net.*;
+import java.io.*;
 /**
  *
- * @author david
+ * @author Andrea
  */
 public class Packet01 {
+    private byte[]  id;
+    private String Alias;
+    private String Message;
+
+    public Packet01(byte[] id, String Alias, String Message) {
+        this.id = id;
+        this.Alias = Alias;
+        this.Message = Message;
+    }
+
+    public byte[] getId() {
+        return id;
+    }
+
+    public void setId(byte[] id) {
+        this.id = id;
+    }
+
+    public String getAlias() {
+        return Alias;
+    }
+
+    public void setAlias(String Alias) {
+        this.Alias = Alias;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+
+    public void setMessage(String Message) {
+        this.Message = Message;
+    }
+    
+    
+    //metodo per creare pacchetto
+    public byte[] createP(){
+        byte[] UserToUser =new byte[2048];
+        
+        int i =0;
+        UserToUser[i++]=01;
+        
+        for (byte b: this.id){
+            UserToUser[i++]=b;
+        }
+        
+        for (byte b: this.Alias.getBytes()){
+            UserToUser[i++]=b;
+        }
+        
+        UserToUser[i++]=0;
+        
+        for (byte b: this.Message.getBytes()){
+            UserToUser[i++]=b;
+        }
+        
+        UserToUser[i++]=0;
+        
+        return UserToUser;
+    }
+   
+    
+    
+    
+        
+    
+    
     
 }
