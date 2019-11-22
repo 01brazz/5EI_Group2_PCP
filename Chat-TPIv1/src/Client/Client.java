21875c7719637149a82898b2d32d8d4e76333d95
BELTRAMI ANDREA <17701@EDU.MARCONI>
2019-11-22 12:38:01 +0100

    Aggiunti metodi nella classe Client

--- Chat-TPIv1/src/Client/Client.java
+++ Chat-TPIv1/src/Client/Client.java
@@ -5,8 +5,12 @@
  */
 package Client;
 
+import pacchetti.Packet01;
+import pacchetti.Packet05;
 import pacchetti.Packet10;
 import pacchetti.Packet11;
+import pacchetti.Packet18;
+import pacchetti.Packet50;
 
 /**
  *
@@ -25,6 +29,22 @@ public class Client {
         return PacchettoLogin;
     };
     
+    public byte[] userToUser(byte[] id,String Alias,String Messagge){
+        
+        Packet01 p = new Packet01(id,Alias,Messagge);
+        byte[] PacchettoUserToUser = p.createP();
+    
+        return PacchettoUserToUser;
+    };
+    
+    public byte[] userToChat(byte[] id,String Messagge){
+        
+        Packet05 p = new Packet05(id,Messagge);
+        byte[] PacchettoUserToChat = p.createP();
+    
+        return PacchettoUserToChat;
+    };
+    
     public byte[] disconnection(byte[] id){
         
         Packet11 p = new Packet11(id);
@@ -32,6 +52,25 @@ public class Client {
     
         return PacchettoDisconnection;
     };
+    
+    public byte[] userListRequest(byte[] id){
+        
+        Packet50 p = new Packet50(id);
+        byte[] PacchettoUserListRequest = p.createP();
+    
+        return PacchettoUserListRequest;
+    };
+    
+    public byte[] changeAlias(byte[] id, String old_Alias, String new_Alias){
+        
+        Packet18 p = new Packet18(id,old_Alias,new_Alias);
+        byte[] PacchettoChangeAlias = p.createP();
+    
+        return PacchettoChangeAlias;
+    };
+    
+    
+    
     
     
     
